782b75652d9ff96366721b1b248472012904991c
Noah Soubliere <nsoubliere@lcs.on.ca>
2022-10-28 15:18:36 -0400

    Added a helper view for showing monochromatic colour palettes

--- ColourSelector/Views/ContentView.swift
+++ ColourSelector/Views/ContentView.swift
@@ -19,29 +19,17 @@ struct ContentView: View {
         return selectedHue / 360.0
     }
     
-    // Make the colour that SwiftUI will use to set the background of the colour swatch
     private var baseColour: Color {
         return Color(hue: hue,
                      saturation: 0.8,
                      brightness: 0.9)
     }
 
-    private var darkerColour: Color {
-        return Color(hue: hue,
-                     saturation: 0.8,
-                     brightness: 0.6)
-    }
-
-    private var evenDarkerColour: Color {
-        return Color(hue: hue,
-                     saturation: 0.8,
-                     brightness: 0.3)
-    }
-
     // Interface
     var body: some View {
         VStack(spacing: 20){
             
+            // Selecting the hue
             HStack{
                 ColourSwatchView(colour: baseColour,
                                  size: 100)
@@ -61,25 +49,9 @@ struct ContentView: View {
                 }
             }
             
-
-            VStack(alignment: .leading){
-                
-                Text("Monochromatic")
-                    .font(.title3)
-                    .bold()
-                
-                HStack(spacing: 0) {
-                    
-                    ColourSwatchView(colour: baseColour,
-                                     size: 50)
-                    ColourSwatchView(colour: darkerColour,
-                                     size: 50)
-                    ColourSwatchView(colour: evenDarkerColour,
-                                     size: 50)
-                    Spacer()
-                    
-                }
-            }
+            //Monochromatic Palette
+            MonochromaticPaletteView(hue: hue)
+          
             Spacer()
             
             
---
+++ ColourSelector/Views/Helpers/MonochromaticPaletteView.swift
@@ -0,0 +1,62 @@
+//
+//  MonochromaticPaletteView.swift
+//  ColourSelector
+//
+//  Created by Noah Alexandre Soubliere on 2022-10-28.
+//
+
+import SwiftUI
+
+struct MonochromaticPaletteView: View {
+    
+    //MARK: Stored Properties
+    var hue: Double
+    //MARK: Computed propeties
+    // Make the colour that SwiftUI will use to set the background of the colour swatch
+    private var baseColour: Color {
+        return Color(hue: hue,
+                     saturation: 0.8,
+                     brightness: 0.9)
+    }
+
+    private var darkerColour: Color {
+        return Color(hue: hue,
+                     saturation: 0.8,
+                     brightness: 0.6)
+    }
+
+    private var evenDarkerColour: Color {
+        return Color(hue: hue,
+                     saturation: 0.8,
+                     brightness: 0.3)
+    }
+
+    
+    var body: some View {
+       
+        VStack(alignment: .leading){
+            
+            Text("Monochromatic")
+                .font(.title3)
+                .bold()
+            
+            HStack(spacing: 0) {
+                
+                ColourSwatchView(colour: baseColour,
+                                 size: 50)
+                ColourSwatchView(colour: darkerColour,
+                                 size: 50)
+                ColourSwatchView(colour: evenDarkerColour,
+                                 size: 50)
+                Spacer()
+                
+            }
+        }
+    }
+}
+
+struct MonochromaticPaletteView_Previews: PreviewProvider {
+    static var previews: some View {
+        MonochromaticPaletteView(hue: 0.0)
+    }
+}
